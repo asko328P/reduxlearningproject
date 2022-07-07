1dbe9e99077c2ef76ee275105bf3807db4e51947
Asad_Basic <asad.basic@protonmail.com>
2022-07-08 00:14:58 +0200

    modified App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,11 @@
 import React from "react";
 
 
-class App extends React.Component {
-    render() {
-        return (
-            <div>
-                App
-            </div>
-        );
-    }
-}
-
+const App = () => {
+    return(
+        <div>
+            App
+        </div>
+    );
+};
 export default App;
