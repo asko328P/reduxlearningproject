61b8cdd16aadb355a7f53ee09b047e27ee5ef30d
Asad_Basic <asad.basic@protonmail.com>
2022-07-08 00:12:09 +0200

    added redux and react redux to project

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 import React from "react";
+import ReactDOM from 'react-dom'
 import {render} from "react-dom";
 import {Provider} from "react-redux";
 import App from "./components/App";
-import configureStore from "./configureStore";
 
-const store = configureStore()
 
-render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.getElementById('root');
-)
+
+ReactDOM.render(<App />, document.querySelector('#root'));
