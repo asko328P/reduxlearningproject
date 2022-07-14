a5d1a400a826b029ba03574a3a6206f720483340
asko328P <52359591+asko328P@users.noreply.github.com>
2022-07-14 15:40:44 +0200

    added prettier with rules (#2)
    Co-authored-by: Asad_Basic <asad.basic@protonmail.com>

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import Login from "./Login";
-
+import Login from './Login';
 
 const App = () => {
-
-    return (
-        <div>
-            <Login />
-        </div>
-    );
-
+  return (
+    <div>
+      <Login />
+    </div>
+  );
 };
 
 export default App;
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,10 +1,10 @@
-import axios from "axios";
-import authHeader from "./auth-header";
-const API_URL = "http://restapi.adequateshop.com/api";
+import axios from 'axios';
+import authHeader from './auth-header';
+const API_URL = 'http://restapi.adequateshop.com/api';
 const getPublicContent = () => {
-    return axios.get(API_URL + "all");
+  return axios.get(API_URL + 'all');
 };
 
 export default {
-    getPublicContent,
+  getPublicContent,
 };
